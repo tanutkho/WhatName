60f0e6f796e5d570af554d6958c7d2f61563ffe7
Kubo <t_nutji@hotmail.com>
2018-06-02 10:51:49 +0700

    Initial Commit

---
+++ WhatName/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatName
+//
+//  Created by Tanut Khositprasert on 2/6/2561 BE.
+//  Copyright © 2561 Tanut Khositprasert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
